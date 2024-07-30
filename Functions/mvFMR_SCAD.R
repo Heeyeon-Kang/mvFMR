52043a17180d2cde3a21c4ac302f8d4bca795039
Heeyeon-Kang <gmldus1163@naver.com>
2024-07-30 13:02:53 +0900

    Update mvFMR_SCAD.R

--- Functions/mvFMR_SCAD.R
+++ Functions/mvFMR_SCAD.R
@@ -13,9 +13,10 @@
 # presented in Section 5.3.
 
 ### Example ###
+# source("./Data/simulation_seed_number.R")
 # source("./Data/simulation_data.R)
-# X <- data_generate_5.3.1(12345)$X
-# Y <- data_generate_5.3.1(12345)$Y
+# X <- data_generate_5.3.1(seed_number_5.3.1[1])$X
+# Y <- data_generate_5.3.1(seed_number_5.3.1[1])$Y
 # mvFMR_SCAD_fit(X, Y, 5)
 
 source("./Functions/functions.R")
