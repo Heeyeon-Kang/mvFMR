eee05f8089ab0ff5935fe963ba594723243907b0
Heeyeon-Kang <gmldus1163@naver.com>
2024-07-30 12:57:17 +0900

    Update simulation_seed_number.R

--- Data/simulation_seed_number.R
+++ Data/simulation_seed_number.R
@@ -16,6 +16,9 @@
 # The seed numbers of Section 5.1, Section 5.2, and Section 5.3 are 
 # 2,5,10 / 210,220,230 / 302,304, respectively, for each model.
 
+# Since we generated 500 independent simulations for each model, the following
+# R-code contains 500 seed numbers for each model.
+
 ### Section 5.1 : model 1 ###
 set.seed(2)
 seed_number_5.1.1 <- sample(1:1e5, 500, replace = FALSE)
