04cff4418cbb7676f0ae7dce060568d934c20c02
Heeyeon-Kang <gmldus1163@naver.com>
2024-07-30 13:04:41 +0900

    Update mvFMR_fixedK.R

--- Functions/mvFMR_fixedK.R
+++ Functions/mvFMR_fixedK.R
@@ -10,12 +10,13 @@
 ##############################################################################
 
 # The following R-code fits the simulation data using mvFMR with fixed K
-# presented in Section 5.1, 5.2, and 5.3.
+# presented in Section 5.1, Section 5.2, and Section 5.3.
 
 ### Example ###
+# source("./Data/simulation_seed_number.R")
 # source("./Data/simulation_data.R)
-# X <- data_generate_5.1.1(12345)$X
-# Y <- data_generate_5.1.1(12345)$Y
+# X <- data_generate_5.1.1(seed_number_5.1.1[1])$X
+# Y <- data_generate_5.1.1(seed_number_5.1.1[1])$Y
 # mvFMR_fixedK_fit(X, Y, 5)
 
 source("./Functions/functions.R")
